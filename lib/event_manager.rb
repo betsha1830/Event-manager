ded4d95d34802611d4f27b6cf2d1299eae73762a
Betsue Weldemariam <btsegay0@gmail.com>
2022-04-25 11:24:52 +0300

    File readlines

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,13 @@
 puts 'Event Manager Initialized!'
 
-if File.exist?('event_attendees.csv')
-  contents = File.read('event_attendees.csv') 
-  puts contents
-else puts "file event_attendees.csv doesn't exist."
+# if File.exist?('event_attendees.csv')
+#   contents = File.read('event_attendees.csv') 
+#   puts contents
+# else puts "file event_attendees.csv doesn't exist."
+# end
+
+lines = File.readlines('event_attendees.csv')
+
+lines.each do |line|
+  puts line
 end
