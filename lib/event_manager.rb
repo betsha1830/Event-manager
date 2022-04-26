bc8dda0cd2eb5fe403d34e1c173ed0b86c301751
Betsue Weldemariam <btsegay0@gmail.com>
2022-04-26 09:54:04 +0300

    manipulating csv files with a built-in csv class

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,4 @@
+require 'csv'
 puts 'Event Manager Initialized!'
 
 # if File.exist?('event_attendees.csv')
@@ -6,9 +7,15 @@ puts 'Event Manager Initialized!'
 # else puts "file event_attendees.csv doesn't exist."
 # end
 
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  p columns
+# lines = File.readlines('event_attendees.csv')
+# lines.each_with_index do |line, index|
+#   next if index == 0
+#   columns = line.split(",")
+#   p columns
+# end
+
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |item|
+  name = item[2]
+  puts name
 end
