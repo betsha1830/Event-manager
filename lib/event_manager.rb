98c5a06f6d1c013c43e63927bc12096d958c5ad8
Betsue Weldemariam <btsegay0@gmail.com>
2022-09-05 09:48:55 +0300

    zipcode moved to a separate function

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,21 +2,25 @@ require 'csv'
 
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
+def clean_zipcode(zipcode)
+
+  if zipcode == nil || zipcode.length < 5
+    zipcode = "" if zipcode.nil?
+    until zipcode.length == 5
+      zipcode = "0" + zipcode
+    end
+  elsif zipcode.length > 5
+    zipcode = zipcode[0,4]
+  end
+
+end
+
 contents.each do |row|
   name = row[:first_name]
-  zip_code = row[:zipcode]
+  zipcode = row[:zipcode]
 
   #if zipcode is five digits then it's a correct zipcode
   #if zipcode is more than five digits chop to the first 5 digits
   #if zipcode is less than five digits fill in with zeros from the beginning
-
-  if zip_code == nil || zip_code.length < 5
-    zip_code = "" if zip_code.nil?
-    until zip_code.length == 5
-      zip_code = "0" + zip_code
-    end
-  elsif zip_code.length > 5
-    zip_code = zip_code[0,4]
-  end
-  puts "#{name} #{zip_code}"
+  puts "#{name} #{zipcode}"
 end
