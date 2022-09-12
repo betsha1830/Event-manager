6d050276701b34a768fed29376cad17176f44a2e
Betsue Weldemariam <btsegay0@gmail.com>
2022-09-12 10:58:50 +0300

    added a function that creates an attendee specific thank you letter

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,8 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
+template_letter = File.read('form_letter.html')
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
 def clean_zipcode(zipcode)
@@ -14,24 +16,33 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(", ")
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
-
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id,form_letter)
 
 end
