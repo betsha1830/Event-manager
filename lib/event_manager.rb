1d14b0d97febd8e3e5e242840d81e70e3f18724c
Betsue Weldemariam <btsegay0@gmail.com>
2022-09-12 11:31:01 +0300

    cleaning phone number input to a valid output

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,8 +6,25 @@ template_letter = File.read('form_letter.html')
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
 def clean_zipcode(zipcode)
-
   zipcode.to_s.rjust(5, '0')[0, 5]
+end
+
+def clean_phone_number(phone_number)
+
+  #If the phone number is less than 10 digits, assume that it is a bad number
+  #If the phone number is 10 digits, assume that it is good
+  #If the phone number is 11 digits and the first number is 1, trim the 1 and use the remaining 10 digits
+  #If the phone number is 11 digits and the first number is not 1, then it is a bad number
+  #If the phone number is more than 11 digits, assume that it is a bad number
+  #Remove any spaces or signs from number
+
+  if phone_number.length < 10 || phone_number.length > 11 || (phone_number.length == 1 && phone_number[0] != '1')
+    ""
+  elsif (phone_number.length == 1 && phone_number[0] == '1') || phone_number.length == 10
+    phone_number
+  end
+
+  phone_number.to_s.rjust()
 
 end
 
@@ -40,6 +57,7 @@ erb_template = ERB.new template_letter
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+  phone_number = row[:HomePhone]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
   form_letter = erb_template.result(binding)
