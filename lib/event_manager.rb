cba7ad5ec5f59f9ea60206b34a17e50498de7581
Betsue Weldemariam <btsegay0@gmail.com>
2022-09-17 16:01:50 +0300

    day of week counter function added

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -59,16 +59,27 @@ def save_thank_you_letter(id,form_letter)
   end
 end
 
-def reg_time_counter()
+def reg_time_counter
   user_reg_at_hours = {}
   attendees = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
   attendees.each do |attendee|
-    puts reg_date = attendee[:regdate]
-    puts date = Time.strptime(reg_date, '%m/%d/%y %H')
+    reg_date = attendee[:regdate]
+    date = Time.strptime(reg_date, '%m/%d/%y %H')
     hour = date.strftime('%k').strip
     user_reg_at_hours.include?(hour) ? user_reg_at_hours[hour] += 1 : user_reg_at_hours[hour] = 1
   end
   user_reg_at_hours
+end
+
+def reg_dow_counter
+  user_reg_dow = {}
+  attendees = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+  attendees.each do |attendee|
+    reg_date = attendee[:regdate]
+    date = Time.strptime(reg_date, '%m/%d/%y').wday
+    user_reg_dow.include?(date) ? user_reg_dow[date] += 1 : user_reg_dow[date] = 1
+  end
+  user_reg_dow
 end
 
 template_letter = File.read('form_letter.erb')
@@ -85,4 +96,4 @@ erb_template = ERB.new template_letter
 
 # end
 
-puts reg_time_counter
+puts reg_dow_counter
